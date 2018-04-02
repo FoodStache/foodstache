9916f14da2f8621b53b0c384f4dc982ee46f1341
Ben Gamber <ben@stickfishstudio.com>
2018-04-02 14:52:23 -0400

    API Route for querying all recipes

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express = require('express');
 const Router = express.Router;
 const bodyParser = require('body-parser');
 
+const db = require('./database');
+
 let app = express();
 let router = new Router();
 
 let api = new Router();
 
+let allRecipes = (req, res) => {
+  getAll = db.getAllRecipes();
+  getAll.then(results => {
+    console.log(results);
+    res.send(results);
+  });
+};
+
+api.get('/recipes', allRecipes);
+
+router.use('/api', api);
 router.use('/', (req, res) => {
   res.send('Hello!');
 });
