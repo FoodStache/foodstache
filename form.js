dd02e1c275c4f473ad6e0d987012e31ce681fc1f
chrisgoodell <cg53142@fastmail.com>
2018-04-03 15:25:28 -0400

    Create add recipe form

---
+++ form.js
@@ -0,0 +1,27 @@
+let form = document.querySelector("form");
+
+let isValidElement = element => element.name && element.value;
+let isValidValue = element => !['checkbox', 'radio'].includes(element.type) || element.checked;
+let isCheckbox = element => element.type === 'checkbox';
+
+let formToJSON = elements => [].reduce.call(elements, (data, element) => {
+  if (isValidElement(element) && isValidValue(element)) {
+    if (isCheckbox(element)) {
+      data[element.name] = (data[element.name] || []).concat(element.value);
+    } else {
+      data[element.name] = element.value;
+    }
+  }
+  return data;
+}, {});
+
+let handleFormSubmit = event => {
+  event.preventDefault();
+  let data = formToJSON(form.elements);
+  //doSomething(data).then(doSomethingElse);
+  //fetch with post and send to 'API/recipes'
+  console.log(data);
+  fetch('api/recipes', {method: 'POST', body: JSON.stringify(data)})
+  };
+
+form.addEventListener('submit', handleFormSubmit);
