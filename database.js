9acb1a28c359eceedfb48fdcf5c195188c86114f
Ben Gamber <ben@stickfishstudio.com>
2018-04-03 11:29:57 -0400

    Add route and query for searching recipes

--- database.js
+++ database.js
@@ -4,6 +4,34 @@ const db = pg(dbConfig);
 
 let getAllRecipes = () => db.query('SELECT * FROM recipes;');
 
+let searchRecipes = (queryParams) => {
+  let whereString = "(";
+  queryParams.forEach((item, index) => {
+    whereString += `title ILIKE '%${item}%'`;
+    if (index !== queryParams.length - 1) {
+      whereString += ' OR ';
+    };
+  });
+  whereString += ') OR (';
+  queryParams.forEach((item, index) => {
+    whereString += `tag ILIKE '%${item}%'`;
+    if (index !== queryParams.length - 1) {
+      whereString += ' OR ';
+    };
+  });
+  whereString += ') OR (';
+  queryParams.forEach((item, index) => {
+    whereString += `ingredients @> 'item: ${item.toLowerCase()}'`;
+    if (index !== queryParams.length - 1) {
+      whereString += ' OR ';
+    };
+  });
+  whereString += ')';
+  console.log(whereString);
+  return db.query(`SELECT * FROM recipes WHERE ${whereString};`);
+};
+
 module.exports = {
-  getAllRecipes: getAllRecipes
+  getAllRecipes: getAllRecipes,
+  searchRecipes: searchRecipes
 };
--- index.js
+++ index.js
@@ -17,12 +17,25 @@ let allRecipes = (req, res) => {
   });
 };
 
+let searchRecipes = (req, res) => {
+  let body = req.body;
+  console.log(body);
+  let queryParams = body["query"];
+  getSearch = db.searchRecipes(queryParams);
+  getSearch.then(results => {
+    console.log(results);
+    res.send(results);
+  });
+};
+
 api.get('/recipes', allRecipes);
+api.post('/recipes/search', searchRecipes);
 
 router.use('/api', api);
 router.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.use(bodyParser.json());
 app.use(router);
 app.listen(3000);
